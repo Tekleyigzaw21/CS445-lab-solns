5dc26a6746efba4edfef82f0762078f9b4e0e909
Tekletsadik Yigzaw <Tyigzaw@miu.edu>
2022-05-11 18:33:54 -0500

    Lab 8 Async await Solution

---
+++ Lab8/exercise01.js
@@ -0,0 +1,27 @@
+"use strict";
+const isPrime = (num) =>
+  new Promise((resolve, reject) => {
+    setTimeout(function () {
+      for (let i = 2, s = Math.sqrt(num); i <= s; i++)
+        if (num % i === 0) reject({ prime: false });
+      resolve({ prime: num > 1 });
+    }, 500);
+  });
+
+async function isPrimeAsync(num) {
+  try {
+    let result = await isPrime(num);
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+  }
+}
+console.log("start");
+isPrime(7)
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err));
+console.log("end");
+// Output:
+// start
+// end
+// { prime: true }
